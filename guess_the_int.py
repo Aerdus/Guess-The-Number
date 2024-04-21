0c0b7c28b9986d5fe4157d4fb25a0814cd4d5480
Aerdus <119413574+Aerdus@users.noreply.github.com>
2024-04-21 12:16:52 +1000

    Version 1
    First version of Guess the Number game

---
+++ guess_the_int.py
@@ -0,0 +1,69 @@
+import random
+import sys
+
+
+def main():
+    score = 0
+    round = 0
+    while True:
+        play_again = input("\nWould you like to play again? Yes/No? ").strip().lower()
+        print("")
+        if play_again == "no":
+            print("\nScore:", score, "\nThanks for playing!")
+            sys.exit(0)
+        elif play_again == "yes":
+            incorrect_guess = 0
+            correct_ans = rand_num()
+            while True:
+                try:
+                    if incorrect_guess == 3:
+                        print("\nGame Over! \nScore:", score)
+                        break
+                    else:
+                        user_input = int(input("Enter a number: "))
+                        if input_check(user_input):
+                            print(int_check(user_input, correct_ans))
+                            if int_check(user_input, correct_ans) == "Correct!":
+                                incorrect_guess == 0
+                                score += 1
+                                round += 1
+                                break
+                            else:
+                                incorrect_guess += 1
+                                round += 1
+                                score = 0
+                        else:
+                            print("Please enter a number between 1 and 10")
+                            continue
+                except ValueError:
+                    print("Please enter a number")
+        else:
+            print("Please only enter Yes/No")        
+
+
+def input_check(user_input):
+    if user_input <= 0 or user_input >= 11:
+        return False
+    else:
+        return True
+
+
+
+def rand_num():
+    return random.randint(1, 10)
+
+
+
+
+def int_check(user_input, rand_num):
+    if rand_num == user_input:
+        return f"\nCorrect!\n"
+    elif rand_num > user_input:
+        return f"\nHigher!\n"
+    elif rand_num < user_input:
+        return f"\nLower!\n"
+    else:
+        return f"\nIncorrect!\n"
+
+
+main()
